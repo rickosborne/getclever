edbae748e679d5a3472e91d7285d6547e6226667
Rick Osborne <rosborne@fullsail.com>
2013-05-19 00:02:29 -0400

    ios:Basic network

--- ios/CleverRick/ViewController.m
+++ ios/CleverRick/ViewController.m
@@ -11,11 +11,18 @@
 @interface ViewController ()
 {
     NSString *apiUrl, *apiHost, *apiKeyDefault;
+    NSMutableString *responseData;
 }
 
 @end
 
 @implementation ViewController
+
+- (void)logAndLog:(NSString*)message
+{
+    NSLog(@"%@", message);
+    txtLog.text = [NSString stringWithFormat:@"%@\n%@", message, txtLog.text];
+}
 
 - (void)viewDidLoad
 {
@@ -24,13 +31,15 @@
     apiKeyDefault = @"DEMO_KEY";
     apiUrl = @"https://api.getclever.com/v1.1/sections";
     apiHost = @"api.getclever.com";
+    responseData = [[NSMutableString alloc] init];
+    txtLog.text = @"";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
 - (void)setFetchState:(Boolean)isFetching
 {
@@ -64,5 +73,60 @@
     }
     return apiKeyDefault;
 }
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    [self logAndLog:[NSString stringWithFormat:@"finished:%d", responseData.length]];
+    NSError* error = nil;
+    NSDictionary* json = [NSJSONSerialization JSONObjectWithData:[responseData dataUsingEncoding:NSUTF8StringEncoding] options:kNilOptions error:&error];
+    if (error == nil)
+    {
+        NSDictionary* data = [json valueForKey:@"data"];
+        if (data)
+        {
+            [self logAndLog:[NSString stringWithFormat:@"got data"]];
+        }
+    }
+    else
+    {
+        [self logAndLog:[NSString stringWithFormat:@"json error:%@", error]];
+    }
+    responseData = [[NSMutableString alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+	[self logAndLog:[NSString stringWithFormat:@"%@ didFailWithError:%@", connection.originalRequest.HTTPMethod, error]];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
+{
+    [self logAndLog:[NSString stringWithFormat:@"didReceiveData:%d", data.length]];
+	if (data.length > 0)
+	{
+        [responseData appendString:[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]];
+	}
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
+{
+    [self logAndLog:[NSString stringWithFormat:@"didReceiveResponse:"]];
+    [self setFetchState:NO];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+{
+    [self logAndLog:[NSString stringWithFormat:@"didReceiveAuthenticationChallenge"]];
+	NSURLCredential* cred = [NSURLCredential credentialWithUser:[self apiKey] password:@"" persistence:NSURLCredentialPersistenceForSession];
+	[challenge.sender useCredential:cred forAuthenticationChallenge:challenge];
+}
+
+- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace
+{
+    [self logAndLog:[NSString stringWithFormat:@"canAuthenticateAgainstProtectionSpace"]];
+	return ([protectionSpace.host isEqualToString:apiHost])
+    && ([protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodDefault]);
+}
+
 
 @end
